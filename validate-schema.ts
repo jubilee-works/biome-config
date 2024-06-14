2563846efecbae3beb9b7df5c5d6d5a839779754
susiyaki <susiyaki.dev@gmail.com>
2024-06-14 14:20:53 +0900

    fix: unit64 validation error

--- validate-schema.ts
+++ validate-schema.ts
@@ -8,31 +8,40 @@ const schema = await response.json();
 const ajv = new Ajv({ strict: false });
 addFormats(ajv);
 
+const isUint8 = (data: unknown): boolean =>
+  typeof data === "number" &&
+  Number.isSafeInteger(data) &&
+  data >= 0 &&
+  data <= 255;
+
+const isUint16 = (data: unknown): boolean =>
+  typeof data === "number" &&
+  Number.isSafeInteger(data) &&
+  data >= 0 &&
+  data <= 65535;
+const isUnit64 = (data: unknown): boolean => {
+  if (typeof data === "number") {
+    return (
+      Number.isSafeInteger(data) && data >= 0 && data <= Number.MAX_SAFE_INTEGER
+    );
+  } else if (typeof data === "bigint") {
+    return data >= 0n && data <= 18446744073709551615n;
+  } else {
+    return false;
+  }
+};
+
 ajv.addFormat("uint8", {
   type: "number",
-  validate: (data: unknown) =>
-    typeof data === "number" &&
-    Number.isInteger(data) &&
-    data >= 0 &&
-    data <= 255,
+  validate: isUint8,
 });
-
 ajv.addFormat("uint16", {
   type: "number",
-  validate: (data: unknown) =>
-    typeof data === "number" &&
-    Number.isInteger(data) &&
-    data >= 0 &&
-    data <= 65535,
+  validate: isUint16,
 });
-
 ajv.addFormat("uint64", {
   type: "number",
-  validate: (data: unknown) =>
-    typeof data === "number" &&
-    Number.isInteger(data) &&
-    data >= 0 &&
-    data <= 18446744073709551615n,
+  validate: isUnit64,
 });
 
 const validate = ajv.compile(schema);
