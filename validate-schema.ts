cfe2d03a5648eb09390034b23fac4986810721f5
susiyaki <susiyaki.dev@gmail.com>
2024-06-14 15:50:02 +0900

    chore: remvoe unit64 validation

--- validate-schema.ts
+++ validate-schema.ts
@@ -8,28 +8,19 @@ const schema = await response.json();
 const ajv = new Ajv({ strict: false });
 addFormats(ajv);
 
-const isUint8 = (data: unknown): boolean =>
-  typeof data === "number" &&
-  Number.isSafeInteger(data) &&
-  data >= 0 &&
-  data <= 255;
-
+const isUint8 = (data: unknown): boolean => {
+  return (
+    typeof data === "number" &&
+    Number.isSafeInteger(data) &&
+    data >= 0 &&
+    data <= 255
+  );
+};
 const isUint16 = (data: unknown): boolean =>
   typeof data === "number" &&
   Number.isSafeInteger(data) &&
   data >= 0 &&
   data <= 65535;
-const isUnit64 = (data: unknown): boolean => {
-  if (typeof data === "number") {
-    return (
-      Number.isSafeInteger(data) && data >= 0 && data <= Number.MAX_SAFE_INTEGER
-    );
-  } else if (typeof data === "bigint") {
-    return data >= 0n && data <= 18446744073709551615n;
-  } else {
-    return false;
-  }
-};
 
 ajv.addFormat("uint8", {
   type: "number",
@@ -38,10 +29,6 @@ ajv.addFormat("uint8", {
 ajv.addFormat("uint16", {
   type: "number",
   validate: isUint16,
-});
-ajv.addFormat("uint64", {
-  type: "number",
-  validate: isUnit64,
 });
 
 const validate = ajv.compile(schema);
