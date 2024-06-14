0c0f151d9cd70e729c2c04580d651ddb9476ad5a
susiyaki <susiyaki.dev@gmail.com>
2024-06-14 16:55:44 +0900

    refactor: validation fn

--- validate-schema.ts
+++ validate-schema.ts
@@ -1,5 +1,5 @@
-import Ajv from "https://esm.sh/ajv@8.12.0";
 import addFormats from "https://esm.sh/ajv-formats@3.0.1";
+import Ajv from "https://esm.sh/ajv@8.12.0";
 import data from "./biome.json" with { type: "json" };
 
 const response = await fetch(data.$schema);
@@ -8,28 +8,16 @@ const schema = await response.json();
 const ajv = new Ajv({ strict: false });
 addFormats(ajv);
 
-const isUint8 = (data: unknown): boolean => {
-  return (
-    typeof data === "number" &&
-    Number.isSafeInteger(data) &&
-    data >= 0 &&
-    data <= 255
-  );
-};
-const isUint16 = (data: unknown): boolean =>
+const isUintN = (n: number) => (data: unknown) =>
   typeof data === "number" &&
   Number.isSafeInteger(data) &&
-  data >= 0 &&
-  data <= 65535;
+  0 <= data &&
+  data < 2 ** n;
 
-ajv.addFormat("uint8", {
-  type: "number",
-  validate: isUint8,
-});
-ajv.addFormat("uint16", {
-  type: "number",
-  validate: isUint16,
-});
+ajv.addFormat("uint8", { type: "number", validate: isUintN(8) });
+ajv.addFormat("uint16", { type: "number", validate: isUintN(16) });
+// Ignore uint64 (for files.maxSize rule)
+ajv.addFormat("uint64", { type: "number", validate: () => true });
 
 const validate = ajv.compile(schema);
 
